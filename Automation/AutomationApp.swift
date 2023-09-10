dfab00eaed0fd243c9f815af6b076f8cca971238
sangtt18 <SangTt18@fpt.com.vn>
2023-09-10 22:47:59 +0700

    Initial Commit

---
+++ Automation/AutomationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomationApp.swift
+//  Automation
+//
+//  Created by Sang Truong on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AutomationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
